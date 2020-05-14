29aff90fc7b12c4250541187277ae99e45e51509
M. A. G <57389247+MarcoAG-115@users.noreply.github.com>
2020-05-14 16:36:29 -0500

    Task 3 (WIP)
    Added code that checks if the file contains a solvable 9x9 Sudoku board. No code has been added to validate the actual contents of the board.

--- main.go
+++ main.go
@@ -1,9 +1,71 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"os"
 )
+
+//Constants
+const boardSize = 171
+
+//Validating the input file.
+//This function uses multiple tests to check if
+//the provided file contents represent a
+//solvable Sudoku board.
+func validator(input string) bool {
+
+	//Variables
+	rowCount := 0
+	testPassed := 0
+	isValid := false
+
+	//Test 1
+	//Checks if the file contains the required number
+	//of characters needed for a 9x9 board. This is
+	//done by finding the size of the file contents.
+	b, err := ioutil.ReadFile(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+	str := string(b)
+	if len(str) != boardSize {
+		fmt.Println("Error: The file contents are not valid. Please use a file with" +
+			"81 integers. (Blanks should be represented with zeros)")
+		testPassed++
+	}
+
+	//Test 2
+	//Checks if the board is organized in a 9x9
+	//configuration. This is done by counting
+	//the number of lines that contain the file's
+	//contents.
+	file, err := os.Open(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		rowCount++
+	}
+
+	if rowCount == 9 {
+		testPassed++
+	} else {
+		fmt.Println("Error: The file contents are not valid. Please organize file contents" +
+			"into a 9x9 board with spaces. (Blanks should be represented with zeros)")
+	}
+
+	if testPassed == 2 {
+		isValid = true
+	}
+
+	return isValid
+}
 
 func main() {
 
@@ -18,11 +80,21 @@ func main() {
 	fmt.Println("Reading", fileName)
 
 	//Reading File
-	//Reads file with given name, checks
-	//if file was opened w/o issues
+	//Reads file with given name &
+	//checks if file was opened w/o issues.
 	data, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Print(string(data))
+
+	//Validating File
+	//The validator function is used to
+	//check if the file contents contain
+	//a solvable Sudoku board.
+	fmt.Println("Checking if file contains a valid board...")
+
+	if validator("file1.txt") == true {
+		fmt.Println("The file contains a valid board.")
+	}
 }
