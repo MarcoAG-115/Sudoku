a036829156cf32863c80ae354e157b99c7791f0d
M. A. G <57389247+MarcoAG-115@users.noreply.github.com>
2020-05-14 18:00:42 -0500

    Task 3
    Adds a validator function that checks if the user's file contains a solvable Sudoku board. The validator function contains three tests that check the file content format and the individual items within the board. If all the tests pass, then the user is told that their board is solvable.

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
+	"strings"
 )
 
 //Constants
@@ -20,19 +22,31 @@ func validator(input string) bool {
 	rowCount := 0
 	testPassed := 0
 	isValid := false
+	var line []string
+	lineCount := 0
+	test3 := 0
 
 	//Test 1
 	//Checks if the file contains the required number
 	//of characters needed for a 9x9 board. This is
 	//done by finding the size of the file contents.
+
+	//Opens file and checks for errors.
 	b, err := ioutil.ReadFile(input)
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	//Converts file contents from bytes to a string.
 	str := string(b)
+
+	//Prints error message if the string containing
+	//the file contents is not equal to 171.
 	if len(str) != boardSize {
 		fmt.Println("Error: The file contents are not valid. Please use a file with" +
 			"81 integers. (Blanks should be represented with zeros)")
+
+	} else {
 		testPassed++
 	}
 
@@ -41,6 +55,8 @@ func validator(input string) bool {
 	//configuration. This is done by counting
 	//the number of lines that contain the file's
 	//contents.
+
+	//Opens file and checks for errors.
 	file, err := os.Open(input)
 	if err != nil {
 		fmt.Println(err)
@@ -49,10 +65,27 @@ func validator(input string) bool {
 
 	scanner := bufio.NewScanner(file)
 
+	//For loop reads file line by line.
 	for scanner.Scan() {
+		//Adds a line from the file to a string array.
+		line = append(line, scanner.Text())
+
+		//Test 3
+		//Each row is also analyzed to
+		//ensure that the board only contains integers
+		//0 through 9.
+		str := strings.ReplaceAll(line[lineCount], " ", "")
+		//Checks if the given string contains an integer.
+		if _, err := strconv.Atoi(str); err == nil {
+			test3++
+		}
+
+		lineCount++
 		rowCount++
 	}
 
+	//Prints error message if the board does not have
+	//nine rows.
 	if rowCount == 9 {
 		testPassed++
 	} else {
@@ -60,7 +93,18 @@ func validator(input string) bool {
 			"into a 9x9 board with spaces. (Blanks should be represented with zeros)")
 	}
 
-	if testPassed == 2 {
+	//Prints error message if the board contains
+	//a non-integer.
+	if test3 == 9 {
+		testPassed++
+	} else {
+		fmt.Println("Error: The file contents are not valid. Please submit a file that only contains" +
+			"the integers 0 through 9.")
+	}
+
+	//The board is determined to be solvable if
+	//it passes all the tests.
+	if testPassed == 3 {
 		isValid = true
 	}
 
@@ -94,7 +138,9 @@ func main() {
 	//a solvable Sudoku board.
 	fmt.Println("Checking if file contains a valid board...")
 
-	if validator("file1.txt") == true {
+	if validator(fileName) == true {
 		fmt.Println("The file contains a valid board.")
+	} else {
+		fmt.Println("Not valid.")
 	}
 }
